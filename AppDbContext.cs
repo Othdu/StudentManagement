c32e3c043eb440fd3ec65009ab1e3f38ba42e76a
Mohamed <moh.mus2019@gmail.com>
2026-05-10 20:35:07 +0300

    Initial commit - EF Core Demo

---
+++ AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreDemo
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+      
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=OTHDU-PC;Database=EFCoreDB;Integrated Security=True;TrustServerCertificate=True;Encrypt=False;");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using EFCoreDemo;
+using Microsoft.EntityFrameworkCore;
+
+using (var db = new AppDbContext())
+{
+    var student = db.Students.Include(s => s.Courses).ToList();
+    foreach(var s in student)
+    {
+        Console.WriteLine($"{s.Name}-{s.Grade}");
+        foreach (var c in s.Courses)
+            Console.WriteLine($" -> {c.CourseName}");
+    }
+    Console.ReadLine();
+}
+    
