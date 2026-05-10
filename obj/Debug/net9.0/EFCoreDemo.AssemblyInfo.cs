44bc45699390fec4c74caa8fee3c6ec5ed9c9671
Mohamed <moh.mus2019@gmail.com>
2026-05-10 21:05:11 +0300

    add README and gitignore

--- obj/Debug/net9.0/EFCoreDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/EFCoreDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc297bb188c592bf8b5d3aefa6aefa20c4ef2d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
