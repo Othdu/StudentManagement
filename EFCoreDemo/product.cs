19b04513372ff0cf5cce13e11b9b3c67a087bf67
Mohamed <moh.mus2019@gmail.com>
2026-05-12 16:33:56 +0300

    bulding an inventory system

---
+++ EFCoreDemo/product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreDemo
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public double price {  get; set; }
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
